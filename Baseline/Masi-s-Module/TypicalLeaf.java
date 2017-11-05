a72ef0b254712b7780d95d07749c9acce6442802
Masi Nazarian <Masea@users.noreply.github.com>
2017-11-04 22:15:50 -0700

    Update and rename Leaf1.java to TypicalLeaf.java

---
+++ Baseline/Masi-s-Module/TypicalLeaf.java
@@ -0,0 +1,45 @@
+package composite;
+
+import java.text.DecimalFormat;
+
+
+public class TypicalLeaf implements IDisplayComponent {
+
+    private String description ;
+    private Double price ;
+
+    public TypicalLeaf (String d, Double p)
+    {
+        description = d ;
+        price = p ;
+    }
+    
+    public void display() {
+	 System.out.println( description );
+    }
+
+    public void addChild(Component c) {
+	// no implementation
+    }
+
+    public void removeChild(Component c) {
+        // no implementation
+    }
+
+    public IDisplayComponent getChild(int i) {
+        // no implementation
+    }
+	
+    public void addSubComponent( IDisplayComponent c ) {
+	// no implementation
+    }
+	
+    public void removeSubComponent( IDisplayComponent c ) {
+	// no implementation
+    }
+	    
+    public IDisplayComponent getSubComponent(int i) {
+	// no implementation
+    }
+	 
+}

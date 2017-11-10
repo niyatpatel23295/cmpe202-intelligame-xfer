7d7a3bd93e02b21d28f7d11c74c1b7fcf544a0a9
Ambika Bohra <bohraambika@yahoo.com>
2017-11-09 22:39:12 -0800

    fixed bug of repeating bananas

--- Baseline/MyWorld.java
+++ Baseline/MyWorld.java
@@ -4,6 +4,8 @@ import greenfoot.GreenfootImage;
 import greenfoot.GreenfootSound;
 import greenfoot.World;
 import java.util.List;
+import java.util.Random;
+
 
 public class MyWorld extends World
 {
@@ -20,10 +22,8 @@ public class MyWorld extends World
   private Trash trash;
   private PowerUp power;
   private HealthScreen health;
-  private ScoreScreen score;
-  private Coin coin;  
-  private Time timer; 
- 
+  private ScoreScreen score; 
+
  public MyWorld()
   {
     super(800, 600, 1);
@@ -33,14 +33,11 @@ public class MyWorld extends World
     
     score = new ScoreScreen();
     health = new HealthScreen(); // added a new health screen to record power level
-    timer = new Time();
     
     trash = new Trash();
     monkey = new Monkey();
     cactus = new Cactus();
     power = new PowerUp();
-    
-    coin =new Coin();
     
     addObject(new Sound(), 740, 570); //sound icon
     addObject(new Menu(), 50, 570); //sound icon
@@ -48,13 +45,14 @@ public class MyWorld extends World
     
     addObject(score, 700, 40);
     addObject(health, 100, 40); // set x, y coordinates
-    addObject(timer, 400, 40);
     
-    addObject(monkey, 100, 480);
-    addObject(trash, 550, 510);
-    putBanana(100, 300, 4);
+    addObject(monkey, 40, 480);
+    addObject(trash, 570, 510);
+    putBanana(60, 300, 2);
+    putBanana(180, 250, 2);
+    
   }
-  
+ 
 
   public void act()
   { 
@@ -62,54 +60,56 @@ public class MyWorld extends World
     if ((((Monkey)getObjects(Monkey.class).get(0)).getX() >= 785 ))
     {   
       trash.setFlag(false); //set flag to false aftor each level
-      cactus.setFlag(false);
-      
-      if (stage == 6) {
-        ((Monkey)getObjects(Monkey.class).get(0)).getMusic().stop();
-        new GreenfootSound("the-end.wav").play(); 
-        stage = 1; //for removing error of background6.png
-        Greenfoot.setWorld(new LevelCompletedState(score));  //directs to level completed world with parameter score   
-      }
-      
-      setBackground(new GreenfootImage("background" + stage + ".png"));
-      ((Monkey)getObjects(Monkey.class).get(0)).setLocation(0, 480);
+      cactus.setFlag(false); //set flag to false aftor each level
+
      
       if (stage == 2) {
-        addObject(trash, 550, 510);
+        this.removeObjects(this.getObjects(Banana.class));//remove previous stage bananas
         addObject(cactus, 200, 510);
-        putBanana(100, 280, 2);
+        putBanana(230, 300, 3);
       }
 
       if (stage == 3) {
+        this.removeObjects(this.getObjects(Banana.class));//remove previous stage bananas
         removeObject(trash);
         removeObject(cactus);
-        addObject(cactus, 228, 500);
-        putBanana(380, 350, 2);
-        addObject(coin, 250, 400); //add coin  
+        addObject(cactus, 235, 510);
+        putBanana(380, 350, 5);
     }
       
       if (stage == 4) {
-        removeObject(coin);
+        this.removeObjects(this.getObjects(Banana.class));//remove previous stage bananas
         removeObject(cactus);
         addObject(trash, 550, 510);
         putBanana(250, 330, 2);
-        putBanana(580, 330, 2);
+        putBanana(580, 330, 1);
         addObject(power, 350, 500);  // To place "power" to get new power up 
       }
       
       
       if (stage == 5) {
+        this.removeObjects(this.getObjects(Banana.class));//remove previous stage bananas
         removeObject(trash);
         removeObject(power); //remove "power" from stage 4 
-        addObject(trash, 550, 510);
-        addObject(cactus, 228, 500);
-        putBanana(250, 450, 3);
-       
+        addObject(trash, 570, 510);
+        addObject(cactus, 220, 500);
+        putBanana(250, 450, 4);
       }
       
-      stage += 1;
+       //level completion    
+      if (stage == 6) {
+        ((Monkey)getObjects(Monkey.class).get(0)).getMusic().stop();
+        new GreenfootSound("the-end.wav").play(); 
+        stage = 1; //for removing error of background
+        Greenfoot.setWorld(new LevelCompletedState(score));  //directs to level completed world with parameter score   
+      }
+      
+      
+       setBackground(new GreenfootImage("background" + stage + ".png"));
+       ((Monkey)getObjects(Monkey.class).get(0)).setLocation(0, 480); //set location after each stage
+       stage++;
     }
-  }
+  } 
   
   public void putBanana(int x, int y, int qtd) {
    
@@ -118,7 +118,5 @@ public class MyWorld extends World
       addObject(new Banana(), x, y);
     }
   }
- 
-
-
+  
 }

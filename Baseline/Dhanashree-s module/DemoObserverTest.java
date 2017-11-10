ed77ca3ebd4a7f4ef652ac87674a5ba076f6dbc3
Dhanashree Gaonkar <dgaonkar17@gmail.com>
2017-11-09 23:45:11 -0800

    Update DemoObserverTest.java

--- Baseline/Dhanashree-s module/DemoObserverTest.java
+++ Baseline/Dhanashree-s module/DemoObserverTest.java
@@ -56,18 +56,3 @@ class BinObserver extends Observer {
         System.out.print(" " + Integer.toBinaryString(subject.getState()));
     }
 }
-
-public class ObserverDemo {
-    public static void main( String[] args ) {
-        Subject sub = new Subject();
-        // Client configures the number and type of Observers
-        new HexObserver(sub);
-        new OctObserver(sub);
-        new BinObserver(sub);
-        Scanner scan = new Scanner(System.in);
-        for (int i = 0; i < 5; i++) {
-            System.out.print("\nEnter a number: ");
-            sub.setState(scan.nextInt());
-        }
-    }
-}

4923eb21060bdf5b898022cc15068fb763951684
Dhanashree Gaonkar <dgaonkar17@gmail.com>
2017-11-10 22:22:15 -0800

    Create OctObserver.java

---
+++ Baseline/Dhanashree-s module/OctObserver.java
@@ -0,0 +1,11 @@
+
+class OctObserver extends Observer {
+    public OctObserver(Subject subject) {
+        this.subject = subject;
+        this.subject.add( this );
+    }
+
+    public void update() {
+        System.out.print(" " + Integer.toOctalString(subject.getState()));
+    }
+}

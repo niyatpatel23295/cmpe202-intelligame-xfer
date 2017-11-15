9053d69c7c0bd2941c9a9ddc4d07dad7e2c34b51
Masea <16696260+Masea@users.noreply.github.com>
2017-11-14 18:01:58 -0800

    added random snack generator functionality based on current level to Factory class

--- Baseline/Masi-s-Module/Factory/Creator.java
+++ Baseline/Masi-s-Module/Factory/Creator.java
@@ -6,6 +6,7 @@ Date: 11/10/2017
 */
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public abstract class Creator {
 
@@ -23,17 +24,24 @@ public abstract class Creator {
         System.out.println( "Building Order: " + this.gameLevel );
         System.out.println( "Extra Snack..." + extra.getClass().getName() );
 
-        snacks.addObject(new Banana());
-        snacks.addObject(new Trash());
-        snacks.addObject(new Cactus());
-        snacks.addObject(extra);
+        ISnack snacks[] = new ISnack(4);
 
-        // generate 4*25 random numbers
-  		// if/else statement: if divisible by ... then banana, and so on
+        snackTypes[0] = new Banana();
+        snackTypes[1] = new Trash();
+        snackTypes[2] = new Cactus();
+        snackTypes[3] = extra;
+
+        // generate 4*25 random numbers (4 stages, 25 snacks each)
         // 40% bananna 25% Trash 25% Cactus 10% Extra
-        // rand(timeseed);
-
-	}
+        Random rand = new Random();
+        int  n;
+        for (int i == 0; i < 100; i++) {
+            n = rand.nextInt(100) + 1; //100 is the maximum and the 1 is our minimum 
+            
+            int index = int(n/40) + int(n/65) + int(n/90) + int(n/100);
+            snacks.addObject(snackTypes[index]);
+        }
+    }
 
 }
 

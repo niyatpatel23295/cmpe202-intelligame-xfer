4198a89e164976e459463b2e979bfa9b7efe5bbe
lkandukuri <lavanya.k9110@gmail.com>
2017-11-18 22:53:05 -0800

    Final updated Strategy pattern

--- Lavanya_Module/FastMode.java
+++ Lavanya_Module/FastMode.java
@@ -1,26 +1,26 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Fast here.
+ * Write a description of class FastMode here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Fast implements GameStrategy
+public class FastMode implements GameStrategy
 {
     static private int speed = 40;    
         
     public int calculateSpeed(){ 
-        if(new ScoreScreen().getScore() == 0){
+        if(ScoreScreen.getInstance().getScore() == 0){
             speed = 40;
         }
-        if(speed > 60){
-            if(new ScoreScreen().getScore() % 12 == 0){
+        if(speed > 55){
+            if(ScoreScreen.getInstance().getScore() % 12 == 0){
                 speed+=2;
             }
         }else{
-            if(new ScoreScreen().getScore() % 8 == 0){
-                speed+=1;
+            if(ScoreScreen.getInstance().getScore() % 8 == 0){
+                speed+=2;
             }
         }
         return speed;

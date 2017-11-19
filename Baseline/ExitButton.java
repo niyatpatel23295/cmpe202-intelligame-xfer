5e2aec3ddabce3cefa7367f39e2c4e35509563de
Dhanashree Gaonkar <dgaonkar17@gmail.com>
2017-11-18 22:11:34 -0800

    Add files via upload

--- Baseline/ExitButton.java
+++ Baseline/ExitButton.java
@@ -2,14 +2,34 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class ExitButton extends Button
 {
-    GameManager gm = GameManager.getInstance();
-	public void act() 
-	{
-	    
-		if (Greenfoot.mouseClicked(this))
-		{
-		    System.out.println("Exit button clicked");
-			gm.stop();
-		}
-	}    
+    //GameManager gm = GameManager.getInstance();
+    
+     private Command command;
+    /**
+     * Act - do whatever the ExitButton wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+        if(Greenfoot.mouseClicked(this)) {
+            invoke();
+        }
+    }  
+    
+ 
+    public void setCommand(Command c){
+        this.command = c;
+    }
+    
+    public void invoke(){
+        this.command.execute();
+    }
+	  
 }
+
+
+
+
+
+  
--- Baseline/HomeWorld.java
+++ Baseline/HomeWorld.java
@@ -26,13 +26,42 @@ public class HomeWorld extends World
         img = new GreenfootImage("menubg1.jpg");
         setBackground(img);
 
-        addObject(new PlayButton(), 400, 250); //takes to play screen
-        addObject(new LevelButton(), 400, 330); //takes to levels screen
-        addObject(new ExitButton(), 400, 410); //stops game
-        addObject(new ScoreButton(), 250, 550); // score board
-        addObject(new ShareFriends(),400, 550); //sharing options
-        addObject(new Help(), 550, 550); //info page for various key operations
+        //addObject(new PlayButton(), 400, 250); //takes to play screen
+        //addObject(new LevelButton(), 400, 330); //takes to levels screen
+        //addObject(new ExitButton(), 400, 410); //stops game
+        //addObject(new ScoreButton(), 250, 550); // score board
+        //addObject(new ShareFriends(),400, 550); //sharing options
+        //addObject(new Help(), 550, 550); //info page for various key operations
+         this.prepare();
         System.out.println("HomeWorld initialized");
+       
+        
+    }
+    
+    public void prepare()
+    {
+        //        GameStart gamestart2 = new GameStart(new Start());
+        //        addObject(gamestart2, 106, 350);
+        PlayDifficultMode pdm = new PlayDifficultMode();
+        addObject(pdm, 400, 330);
+        
+        PlayEasyMode pem = new PlayEasyMode();
+        addObject(pem, 400, 250);
+        
+        ExitButton eb = new ExitButton();
+        addObject(eb, 400, 410); //stops game
+        
+        Command commandSlow = new ConcreteCommand();
+        Command commandFast = new ConcreteCommand();
+        Command commandExit = new ConcreteCommand();
+        
+        commandSlow.setReceiver(new ReceiverSlow());
+        commandFast.setReceiver(new ReceiverFast());
+        commandExit.setReceiver(new ReceiverExit());
+        
+        pdm.setCommand(commandFast);
+        pem.setCommand(commandSlow);
+        eb.setCommand(commandExit);
         
     }
 }

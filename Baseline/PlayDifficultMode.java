5feb5f8f3be4c33e055d7cb3bcc6bb7174aeb346
lkandukuri <lavanya.k9110@gmail.com>
2017-11-26 17:21:18 -0800

    Update

--- Baseline/PlayDifficultMode.java
+++ Baseline/PlayDifficultMode.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class PlayDifficultMode extends Button
+public class PlayDifficultMode extends Button implements Invoker
 {
     private Command command;
     //private GreenfootSound music;
